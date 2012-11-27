a52f328b4c7d24dc22cf4a920016fd8edd4d1976
Ryan J <ryan@roflopoly.com>
2012-11-27 14:17:23 -0500

    Shuffle around files; use single transaction when populating cities.db

--- data/GenerateDB.rb
+++ data/GenerateDB.rb
@@ -1,30 +1,32 @@
 # This takes in the tab-delimited file and
 # shoves it into the db
-
-require 'sqlite3'
 require 'time'
+require 'sqlite3'
 
-db = SQLite3::Database.new "data/cities2.db"
+db_filepath = "cities.db"
+db = SQLite3::Database.new db_filepath
 db.execute <<-SQL
   create table if not exists cities (
     name varchar(200),
     population bigint,
     timezone varchar(80)
-  );
+ );
 SQL
 
+db.execute "delete from cities"
 city_insert = db.prepare("insert into cities(name, population, timezone) values(?,?,?)")
-
-cities = File.new("data/cities15000.txt", "r")
+cities = File.new "cities15000.txt"
+num_rows_inserted = 0
+db.transaction
 start_time = Time.now
 while (city = cities.gets)
   split = city.split("\t")
   utf8_name = split[1]
   population = split[14]
   timezone = split[17]
-  #db.execute("insert into cities(name, population, timezone) values(?,?,?)",utf8_name,population,timezone)
   city_insert.execute(utf8_name, population,timezone)
-  puts "inserted #{split[0]}"
+  num_rows_inserted += 1
 end
+db.commit
 end_time = Time.now
-puts "Took #{(end_time - start_time) * 1000}"
+puts "Took #{(end_time - start_time) * 1000} seconds to insert #{num_rows_inserted} records."
