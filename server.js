71384c60c8e035e3f0d7bff95b030c9ff46f2592
Ryan J <ryan@roflopoly.com>
2023-07-13 16:40:29 -0400

    search endpoint

--- server.js
+++ server.js
@@ -4,10 +4,18 @@ const Hapi = require('@hapi/hapi');
 const Inert = require('@hapi/inert');
 const Path = require('path');
 const { DateTime } = require("luxon");
+const Tzc = require('timezonecomplete');
+const sqlite3 = require('sqlite3').verbose();
+const { promisify } = require('util');
 
 const init = async () => {
 
+  //sqlite3 shit
+  const cities = new sqlite3.Database(`${__dirname}/data/cities.db`);
+  const searchQuery = cities.prepare('select * from cities where name LIKE ? order by population desc');
 
+  //IANA tz database
+  const ianaDb = Tzc.TzDatabase.instance();
 
   const server = Hapi.server({
     port: 9393,
@@ -57,11 +65,53 @@ const init = async () => {
     }
   });
 
+  function transformRow(row) {
+    /**
+     * {
+     *  "city":"New York City",
+     *  "offset":-18000,
+     *  "dstOffset":-14400,
+     *  "inDst":true,
+     *  "country":"NY, United States",
+     *  "nextTimeChange":"2023-11-05T02:00:00+00:00"}
+     */
+
+    const currentZoneName = row.timezone;
+    const currentTimeInZone = DateTime.now().setZone(currentZoneName);
+    let a = {
+      city: row.name,
+      inDST: currentTimeInZone.isInDST,
+      offset: ianaDb.standardOffset(currentZoneName, Date.now()).as(Tzc.TimeUnit.Second),
+      dstOffset: ianaDb.totalOffset(currentZoneName, Date.now()).as(Tzc.TimeUnit.Second),
+      offsetNameShort: currentTimeInZone.offsetNameShort,
+      country: row.country,
+      nextTimeChange: DateTime.fromMillis(ianaDb.nextDstChange(currentZoneName, Date.now())).toUTC()
+    };
+
+    console.log(a);
+    return a;
+  }
+
   server.route({
     method: 'GET',
     path: '/search',
     handler: (request, h) => {
-      return 'Hello World!';
+
+      return new Promise((resolve, reject) => {
+        let city = request.query['q'];
+        console.log(city);
+        searchQuery.all(city, function (err, rows) {
+          if (err) reject(err);
+
+          console.log('rows', rows);
+          let results = rows.map(r => {
+            console.log('r', r);
+            return transformRow(r);
+          });
+          return resolve({ results: results });
+        });
+      });
+
     }
   });
 
